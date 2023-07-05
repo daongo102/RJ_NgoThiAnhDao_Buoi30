17964677ce67d4493c04266c61d440195e99193a
NgoThiAnhDao <anhdaongo1092@gmail.com>
2023-07-05 23:18:26 +0700

    update code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import RccComponent from "./module/tao-component/rcc-component";
+
+function App() {
+   return (
+      <>
+         <h1>Hello World</h1>
+         <RccComponent/>
+      </>
+   );
+}
+
+export default App;
---
+++ src/module/tao-component/rcc-component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class RccComponent extends Component {
+   render() {
+      return (
+         <div>
+            <p>Tự định nghĩa</p>
+            <button className="btn btn-success">Click Me</button>
+         </div>
+      );
+   }
+}
